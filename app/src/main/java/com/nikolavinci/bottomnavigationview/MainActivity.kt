44c71e2d1ce5bcc1d5e1fc02f9445d20654f1c2f
Anil <anilbhattarai2003@gmail.com>
2020-10-11 17:18:45 +0545

    Merging NavigationView and NavController

--- app/src/main/java/com/nikolavinci/bottomnavigationview/MainActivity.kt
+++ app/src/main/java/com/nikolavinci/bottomnavigationview/MainActivity.kt
@@ -2,10 +2,23 @@ package com.nikolavinci.bottomnavigationview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
+        val appBarConfiguration =
+            AppBarConfiguration(setOf(R.id.firstFragment, R.id.secondFragment, R.id.thirdFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
